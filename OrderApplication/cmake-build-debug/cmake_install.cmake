4cea739a0c5eea5215b51c1f8b12db2da7b1c755
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2022-11-02 19:17:43 -0400

    Added includes.

--- OrderApplication/cmake-build-debug/cmake_install.cmake
+++ OrderApplication/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/luiscabrera/One Drive/OneDrive - Florida Gulf Coast University/OrderApplication
+# Install script for directory: C:/Users/jcubm/OneDrive - Florida Gulf Coast University/School/Year 2/Fall/COP 3003/Group Project/OrderApplication/OrderApplication
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OrderApplication")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/luiscabrera/One Drive/OneDrive - Florida Gulf Coast University/OrderApplication/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jcubm/OneDrive - Florida Gulf Coast University/School/Year 2/Fall/COP 3003/Group Project/OrderApplication/OrderApplication/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- OrderApplication/main.cpp
+++ OrderApplication/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
